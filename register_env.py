33b1e5c8247bc3a2e072d61005cd980e7ecef550
Supriyak957 <103773973+Supriyak957@users.noreply.github.com>
2025-04-05 13:00:40 +0530

    Create register_env.py

---
+++ register_env.py
@@ -0,0 +1,9 @@
+import gymnasium as gym
+from gymnasium.envs.registration import register
+
+# Register the custom environment
+register(
+    id='TrafficLight-v0',
+    entry_point='traffic_env:TrafficLightEnv',
+    max_episode_steps=1000,
+)
